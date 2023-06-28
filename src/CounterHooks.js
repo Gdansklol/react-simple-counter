4dd74303be4183ab78b43ad8b07d3ae9806677aa
Båmi Lee <swedenebomi@gmail.com>
2023-06-28 18:53:44 +0200

    add function component CounterHooks.js

---
+++ src/CounterHooks.js
@@ -0,0 +1,13 @@
+//Project One - Counter Application
+// 06 - Funtion Component
+
+import React from 'react';
+
+export default function CounterHooks() {
+    return (
+        <div>
+            <button>-</button>
+        </div>
+    )
+}
+
